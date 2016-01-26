383237511c6af7c45614781d941229d6460d5abf
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-01-26 14:34:20 +0100

    fix error function getArticles

--- model.php
+++ model.php
@@ -3,13 +3,13 @@
 /**
  * This function return all the articles of the blog
  */
-public function getArticles()
+function getArticles()
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=microcms;charset=utf8', 
 	               'microcms_user', 
 	               'secret');
-	
+
 	$articles = $bdd->query('select * from t_article order by art_id desc');
-	
+
 	return $articles;
 }
